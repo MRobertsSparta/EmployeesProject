71f19cac7976e51775be7d7ed1487e09dded6980
Max Roberts <MRoberts@spartaglobal.com>
2022-10-24 17:09:47 +0100

    Added employee object

---
+++ src/main/java/com/sparta/App.java
@@ -0,0 +1,9 @@
+package com.sparta;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
---
+++ src/main/java/com/sparta/Employee.java
@@ -0,0 +1,97 @@
+package com.sparta;
+
+import java.util.Date;
+
+public class Employee {
+
+    private int id;
+    private String title; //Could be changed to Enum
+    private String firstName;
+    private String lastName;
+    private String middleInitial;
+    private char gender; //Could be changed to Enum
+    private String email;
+    private Date dateOfBirth;
+    private Date dateOfJoining;
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(Date dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
