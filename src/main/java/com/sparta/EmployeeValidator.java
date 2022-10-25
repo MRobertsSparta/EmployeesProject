530245fb9ed0d9c0c71da77e57c2f89fa8a88e4e
Max Roberts <MRoberts@spartaglobal.com>
2022-10-25 16:57:43 +0100

    Added EmployeeValidator Class

---
+++ src/main/java/com/sparta/EmployeeValidator.java
@@ -0,0 +1,67 @@
+package com.sparta;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.TreeSet;
+
+public class EmployeeValidator {
+
+    public static void validateAll(ArrayList<Employee> employees) {
+        TreeSet<Employee> set = new TreeSet<>(new Employee.EmployeeComparator());
+        ArrayList<Employee> corruptions = new ArrayList<>();
+        for (Employee employee: employees) {
+            if (!isValid(employee) || !set.add(employee)) {
+                corruptions.add(employee);
+            }
+        }
+    }
+
+    public static boolean isValid(Employee employee) {
+        return (
+                validateTitle(employee.getTitle())
+                && validateName(employee.getFirstName())
+                && validateName(employee.getLastName())
+                && validateMiddleInitial(employee.getMiddleInitial())
+                && validateGender(employee.getGender())
+                && validateEmail(employee.getEmail())
+                && validateDate(employee.getDateOfBirth())
+                && validateDate(employee.getDateOfJoining())
+                && validateJoinedWhenOver18(employee.getDateOfBirth(), employee.getDateOfJoining())
+                && validateSalary(employee.getSalary())
+                );
+    }
+
+    private static boolean validateTitle(String title) {
+        return title.matches("(M(rs?|s)|Drs?|Prof|Hon)\\.");
+    }
+
+    private static boolean validateName(String name) {
+        return name.matches("[A-Za-z]+");
+    }
+
+    private static boolean validateMiddleInitial(String initial) {
+        return initial.matches("[A-Z]?");
+    }
+
+    private static boolean validateGender(char gender) {
+        return (gender + "").matches("[FM]");
+    }
+
+    private static boolean validateEmail(String email) {
+        return email.matches(".+@.+\\..+");
+    }
+
+    private static boolean validateDate(Date date) {
+        return date.before(new Date());
+    }
+
+    private static boolean validateJoinedWhenOver18(Date dateOfBirth, Date dateOfJoining) {
+        return (dateOfJoining.getTime() - dateOfBirth.getTime() > 568025136000l); //18 years = 568025136000 milliseconds
+    }
+
+    private static boolean validateSalary(int salary) {
+        return (salary > 0);
+    }
+
+}
