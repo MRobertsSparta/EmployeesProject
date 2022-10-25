6f566ec71c79a27ac78903e202208301ea787449
Max Roberts <MRoberts@spartaglobal.com>
2022-10-25 15:28:16 +0100

    Added logging classes

---
+++ src/main/java/com/sparta/logging/CustomFileHandler.java
@@ -0,0 +1,27 @@
+package com.sparta.logging;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+
+public class CustomFileHandler {
+
+    public static FileHandler fileHandler;
+
+    static {
+        try {
+            LocalDateTime time = LocalDateTime.now();
+            String url = "src/main/resources/sort_manager_log.log";
+            fileHandler = new FileHandler(url, true);
+            fileHandler.setLevel(Level.ALL);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static FileHandler getFileHandler() {
+        return fileHandler;
+    }
+}
