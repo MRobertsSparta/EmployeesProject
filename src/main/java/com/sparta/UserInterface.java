6cfcc646e20a5025393234ab294ed78c79cccb50
lcy <cyleung1212@gmail.com>
2022-10-26 14:55:01 +0100

    Updated UI class

--- src/main/java/com/sparta/UserInterface.java
+++ src/main/java/com/sparta/UserInterface.java
@@ -1,28 +1,16 @@
 package com.sparta;
 
-import java.util.List;
-
 public class UserInterface {
-    static List<Employee> cleanRecord;
-    static List<Employee> corruptedRecord;
 
-    static int numberOfCleanRecord;
-    static int numberOfCorruptedRecord;
-    static int numberOfDuplicateRecord;         //records with duplicate id
-    static int numberOfInvalidRecord;         //records with gender X, middleInitial FALSE, invalid date
-
-    static int timeBeforeMultithread;
-    static int timeAfterMultithread;
-
-    public static void print(){
+    public static void print(EmployeeRecords records){
         System.out.println("----Results of Reading the Employee CSV File----");
-        System.out.println("Number of clean records: " + numberOfCleanRecord);
-        System.out.println("Number of corrupted records: " + numberOfCorruptedRecord);
-        System.out.println("\t- " + numberOfDuplicateRecord + " records are duplicate in employee ID");
-        System.out.println("\t- " + numberOfInvalidRecord + " records contain invalid fields");
+        System.out.println("Number of clean records: " + records.getCleanRecords().size();
+        System.out.println("Number of corrupted records: " + records.getCorruptRecords().size();
+        System.out.println("\t- " + records.getDuplicateRecords() + " records are duplicate in employee ID");
+        System.out.println("\t- " + records.InvalidRecords() + " records contain invalid fields");
 
         System.out.println("\n----Results of Multithread Speed Test----");
-        System.out.println("Time taken before implementing multiple threads: " + timeBeforeMultithread);
-        System.out.println("Time taken after implementing multiple threads: " + timeAfterMultithread);
+        System.out.println("Time taken before implementing multiple threads: " + records.getTimeBeforeMultithread());
+        System.out.println("Time taken after implementing multiple threads: " + records.getTimeAfterMultithread());
     }
 }
