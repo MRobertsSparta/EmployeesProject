195a72a3c90a3b497f36259c455dcbd48741bb6e
lcy <cyleung1212@gmail.com>
2022-10-26 16:10:49 +0100

    Deleted timing part

--- src/main/java/com/sparta/UserInterface.java
+++ src/main/java/com/sparta/UserInterface.java
@@ -6,11 +6,7 @@ public class UserInterface {
         System.out.println("----Results of Reading the Employee CSV File----");
         System.out.println("Number of clean records: " + records.getCleanRecords().size();
         System.out.println("Number of corrupted records: " + records.getCorruptRecords().size();
-        System.out.println("\t- " + records.getDuplicateRecords() + " records are duplicate in employee ID");
-        System.out.println("\t- " + records.InvalidRecords() + " records contain invalid fields");
-
-        System.out.println("\n----Results of Multithread Speed Test----");
-        System.out.println("Time taken before implementing multiple threads: " + records.getTimeBeforeMultithread());
-        System.out.println("Time taken after implementing multiple threads: " + records.getTimeAfterMultithread());
+        System.out.println("\t- " + records.getDuplicateRecords().size() + " records are duplicate in employee ID");
+        System.out.println("\t- " + records.InvalidRecords().size() + " records contain invalid fields");
     }
 }
