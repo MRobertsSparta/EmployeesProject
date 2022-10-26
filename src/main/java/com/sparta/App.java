063121da4cc783311d2e710173784c2c7e69c669
HamzMas <32570820+HamzMas@users.noreply.github.com>
2022-10-26 16:05:48 +0100

    Added CSVReaderTest class and clean up App class

--- src/main/java/com/sparta/App.java
+++ src/main/java/com/sparta/App.java
@@ -1,9 +1,5 @@
 package com.sparta;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-
 public class App
 {
     public static void main( String[] args )
